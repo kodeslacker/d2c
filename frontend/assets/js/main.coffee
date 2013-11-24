cdd00beb7ab7e27f9e861c8ddc00311f37786088
Theodor Vararu <theo@tvararu.ro>
2013-11-24 02:35:31 +0200

    Finish media section

--- frontend/assets/js/main.coffee
+++ frontend/assets/js/main.coffee
@@ -17,6 +17,10 @@ toastr.options = {
   "hideMethod": "fadeOut"
 }
 
+window.App = {}
+# App.backendPath = "http://172.28.101.19:9393/"
+App.backendPath = "http://192.168.0.107:9393/"
+
 domus = angular.module("domus", [])
 domus.config ["$routeProvider", ($routeProvider) ->
   $routeProvider.when "/",
@@ -28,29 +32,45 @@ domus.config ["$routeProvider", ($routeProvider) ->
   $routeProvider.when "/dashboard/maintenance",
     templateUrl: "maintenance.html"
     controller: maintenanceController
+  $routeProvider.when "/dashboard/media",
+    templateUrl: "media.html"
+    controller: mediaController
 ]
 
 consumerUpdateController = ($scope, $http) ->
-  $http.get("http://172.28.101.19:9393/getAllConsumerUpdate").success (data) ->
+  $http.get("getAllConsumerUpdate").success (data) ->
     console.log data[0].attributes
 
 
 domusController = ($scope, $http, $location) ->
   $scope.getDomusCode = ->
-    $http.get("http://172.28.101.19:9393/getDomusCode").success (data) ->
+    $http.get(App.backendPath + "getDomusCode").success (data) ->
       $scope.code = data
 
 
   $scope.checkDomusCode = ->
-    $http.get("http://172.28.101.19:9393/checkDomusCode/" + $scope.code).success (data) ->
+    $http.get(App.backendPath + "checkDomusCode/" + $scope.code).success (data) ->
       if data is "true"
         $location.path '/dashboard'
 
 maintenanceController = ($scope, $http, $location) ->
   $scope.getConsumers = ->
-    $http.get("http://172.28.101.19:9393/getAllConsumers").success (data) ->
+    $http.get(App.backendPath + "getAllConsumers").success (data) ->
       $scope.consumers = data
   
   $scope.updateConsumer = (item) ->
-    $http.post("http://172.28.101.19:9393/submitConsumerUpdate", item).success (data) ->
+    $("#consumer-" + item.id).button('loading')
+    $http.post(App.backendPath + "submitConsumerUpdate/", item).success (data) ->
+      toastr[data.type](data.title, data.message)
+      $scope.getConsumers()
+
+mediaController = ($scope, $http, $location) ->
+  $scope.search = ->
+    $http.get(App.backendPath + "getVideos/" + $scope.query).success (data) ->
+      $scope.videos = data
+  
+  $scope.sendYoutube = (item) ->
+    console.log item
+    toastr["info"]("The video should start playing shortly.", "Youtube request sent!")
+    $http.post(App.backendPath + "submitYoutubeUpdate/", item).success (data) ->
       toastr[data.type](data.title, data.message)
--- frontend/public/js/main.js
+++ frontend/public/js/main.js
@@ -1,5 +1,5 @@
 (function() {
-  var consumerUpdateController, domus, domusController, maintenanceController;
+  var consumerUpdateController, domus, domusController, maintenanceController, mediaController;
 
   console.log("Javascript loaded");
 
@@ -20,6 +20,10 @@
     "hideMethod": "fadeOut"
   };
 
+  window.App = {};
+
+  App.backendPath = "http://192.168.0.107:9393/";
+
   domus = angular.module("domus", []);
 
   domus.config([
@@ -32,27 +36,31 @@
         templateUrl: "dashboard.html",
         controller: domusController
       });
-      return $routeProvider.when("/dashboard/maintenance", {
+      $routeProvider.when("/dashboard/maintenance", {
         templateUrl: "maintenance.html",
         controller: maintenanceController
+      });
+      return $routeProvider.when("/dashboard/media", {
+        templateUrl: "media.html",
+        controller: mediaController
       });
     }
   ]);
 
   consumerUpdateController = function($scope, $http) {
-    return $http.get("http://172.28.101.19:9393/getAllConsumerUpdate").success(function(data) {
+    return $http.get("getAllConsumerUpdate").success(function(data) {
       return console.log(data[0].attributes);
     });
   };
 
   domusController = function($scope, $http, $location) {
     $scope.getDomusCode = function() {
-      return $http.get("http://172.28.101.19:9393/getDomusCode").success(function(data) {
+      return $http.get(App.backendPath + "getDomusCode").success(function(data) {
         return $scope.code = data;
       });
     };
     return $scope.checkDomusCode = function() {
-      return $http.get("http://172.28.101.19:9393/checkDomusCode/" + $scope.code).success(function(data) {
+      return $http.get(App.backendPath + "checkDomusCode/" + $scope.code).success(function(data) {
         if (data === "true") {
           return $location.path('/dashboard');
         }
@@ -62,12 +70,29 @@
 
   maintenanceController = function($scope, $http, $location) {
     $scope.getConsumers = function() {
-      return $http.get("http://172.28.101.19:9393/getAllConsumers").success(function(data) {
+      return $http.get(App.backendPath + "getAllConsumers").success(function(data) {
         return $scope.consumers = data;
       });
     };
     return $scope.updateConsumer = function(item) {
-      return $http.post("http://172.28.101.19:9393/submitConsumerUpdate", item).success(function(data) {
+      $("#consumer-" + item.id).button('loading');
+      return $http.post(App.backendPath + "submitConsumerUpdate/", item).success(function(data) {
+        toastr[data.type](data.title, data.message);
+        return $scope.getConsumers();
+      });
+    };
+  };
+
+  mediaController = function($scope, $http, $location) {
+    $scope.search = function() {
+      return $http.get(App.backendPath + "getVideos/" + $scope.query).success(function(data) {
+        return $scope.videos = data;
+      });
+    };
+    return $scope.sendYoutube = function(item) {
+      console.log(item);
+      toastr["info"]("The video should start playing shortly.", "Youtube request sent!");
+      return $http.post(App.backendPath + "submitYoutubeUpdate/", item).success(function(data) {
         return toastr[data.type](data.title, data.message);
       });
     };
